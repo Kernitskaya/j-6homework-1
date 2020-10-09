6f77d3fbd8ecd60f10482aeeeb1898091f3e4ef3
Julia Kernitckaya <kernyusha@yandex.ru>
2020-10-09 07:45:57 +0300

    add parameterized test

--- src/test/java/ru/testing/statistics/StatsServiceTest.java
+++ src/test/java/ru/testing/statistics/StatsServiceTest.java
@@ -1,6 +1,11 @@
 package ru.testing.statistics;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,45 +13,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class StatsServiceTest {
 
     static final int[] NUMBERS = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    private StatsService statsService = new StatsService();
+
+    public static Stream<Arguments> averageArguments() {
+        return Stream.of(
+                Arguments.of(new int[] {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18}, 15.0),
+                Arguments.of(new int[] {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 16}, 14.83)
+        );
+    }
 
     @Test
     void testSum() {
-        StatsService statsService = new StatsService();
         final int expected = 180;
         assertEquals(expected, statsService.salesSum(NUMBERS));
     }
 
-    @Test
-    void testAverage() {
-        StatsService statsService = new StatsService();
-        final double expected = 15.0;
-        assertEquals(expected, statsService.salesAverage(NUMBERS));
+    @ParameterizedTest
+    @MethodSource("averageArguments")
+    void testAverage(int[] array, double expected) {
+        assertEquals(expected, statsService.salesAverage(array));
     }
 
     @Test
     void testGetMonthIndexWithMaxSale() {
-        StatsService statsService = new StatsService();
         final int expected = 8;
         assertEquals(expected, statsService.getMonthIndexWithMaxSale(NUMBERS));
     }
 
     @Test
     void testMonthWithMinSale() {
-        StatsService statsService = new StatsService();
         final int expected = 9;
         assertEquals(expected, statsService.getMonthIndexWithMinSale(NUMBERS));
     }
 
     @Test
     void testGetMonthCountLessAvg() {
-        StatsService statsService = new StatsService();
         final int expected = 5;
         assertEquals(expected, statsService.getMonthCountLessAvg(NUMBERS));
     }
 
     @Test
     void testGetMonthCountGreaterAvg() {
-        StatsService statsService = new StatsService();
         final int expected = 5;
         assertEquals(expected, statsService.getMonthCountGreaterAvg(NUMBERS));
     }
